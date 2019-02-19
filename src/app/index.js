6d90ab333dbf2faacaec7b9e94be0b4fb8827577
Mindaugas Jacionis <mindaugas.jacionis@tesonet.com>
2019-02-19 23:19:45 +0200

    Code quality setup & Header part of CV page

--- src/app/index.js
+++ src/app/index.js
@@ -1,22 +1,34 @@
 import React from 'react';
-import './index.css';
+import './index.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img 
-          src="https://media.giphy.com/media/2inElen0t36MJ9vSSy/giphy.gif" 
-          className="App-logo" 
-          alt="logo" 
+      <header className="Header">
+        <h1>John Snow</h1>
+        <hr />
+        <p className="Header--contact">Phone: +37063339991</p>
+        <p className="Header--contact">
+          Email:{' '}
+          <a
+            href="mailto:John@Snow.hbo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            John@Snow.hbo
+          </a>
+        </p>
+        <p className="Header--summary">
+          Wafer apple pie cheesecake tootsie roll sweet roll jelly-o liquorice
+          jujubes. Dessert carrot cake danish lollipop. Oat cake tart chupa
+          chups pie toffee donut. Liquorice brownie lollipop bonbon carrot cake
+          candy canes.
+        </p>
+        <img
+          className="Header--profile-picture"
+          alt="This is me"
+          src="https://media.licdn.com/dms/image/C4D03AQE4FlH9z1QruA/profile-displayphoto-shrink_200_200/0?e=1554336000&v=beta&t=gLOCgXaXN7ynK05-jwSotpEs1Qnz0giDsbGa4hT5Ms0"
         />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          </p>
-        <p>Hello My Name is: Mindaugas</p>
-        <button onClick={() => alert('My Button is Cool')}>
-          Button
-        </button>
       </header>
     </div>
   );
